539ba853d990b12c1a1a6bc4637cb2ec8dfb7335
Rafaela Wolf <rafaelawolf52@gmail.com>
2024-12-06 11:12:35 -0300

    ignorando arquivos e refatorando o código

--- src/js/index.js
+++ src/js/index.js
@@ -1,32 +1,52 @@
-function feedbackNumberSelected() {
+// function feedbackNumberSelected() {
+//     const selectedNumber = document.querySelectorAll('.number');
+//     let isFeedbcakSelected = true;
+
+//     selectedNumber.forEach(number => {
+//         number.addEventListener('click', () => {
+//             if (!number.classList.contains("selected")) {
+//                 selectedNumber.forEach(num => num.classList.remove("selected"));
+//                 number.classList.add("selected");
+//                 isFeedbcakSelected = true;
+//             } else {
+//                 number.classList.remove("selected");
+//                 isFeedbcakSelected = false;
+//             }
+//         });
+//     });
+
+//     return isFeedbcakSelected
+// }
+
+// const submitButton = document.querySelector(".btn");
+
+// const hideFirstCard = document.querySelector(".main-card");
+
+// const showSecondCard = document.querySelector('.hidden-card');
+
+// if (feedbackNumberSelected()) {
+//     submitButton.addEventListener('click', () => {
+//         hideFirstCard.classList.add("hide-card");
+//         showSecondCard.classList.add("show-hidden-card");
+//     })
+// }
+
+const submitBtn = document.querySelector('.submit-btn');
+
+
+
+submitBtn.addEventListener('click', (event) => {
+    event.preventDefault();
+
     const selectedNumber = document.querySelectorAll('.number');
-    let isFeedbcakSelected = true;
 
     selectedNumber.forEach(number => {
         number.addEventListener('click', () => {
-            if (!number.classList.contains("selected")) {
-                selectedNumber.forEach(num => num.classList.remove("selected"));
-                number.classList.add("selected");
-                isFeedbcakSelected = true;
-            } else {
-                number.classList.remove("selected");
-                isFeedbcakSelected = false;
+            if (selectedNumber) {
+                selectedNumber.classList.add('selected');
             }
-        });
-    });
-
-    return isFeedbcakSelected
-}
-
-const submitButton = document.querySelector(".btn");
-
-const hideFirstCard = document.querySelector(".main-card");
-
-const showSecondCard = document.querySelector('.hidden-card');
-
-if (feedbackNumberSelected()) {
-    submitButton.addEventListener('click', () => {
-        hideFirstCard.classList.add("hide-card");
-        showSecondCard.classList.add("show-hidden-card");
+    
+            selectedNumber.classList.remove('selected');
+        })
     })
-}
+});
